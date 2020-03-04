9abb3ff6c96ff128be45331a5474ef84dddbc698
ait ihya <hajaraitihya@gmail.com>
2020-03-04 10:57:38 +0100

    use logger and adding dependencies for slf4j

--- exercice5/src/main/java/fr/uvsq/exercice5/Aaffichage.java
+++ exercice5/src/main/java/fr/uvsq/exercice5/Aaffichage.java
@@ -1,13 +1,21 @@
 package fr.uvsq.exercice5;
 
+import java.lang.invoke.MethodHandles;
 import java.time.LocalDateTime;
+
+import org.apache.log4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 public class Aaffichage implements Affichage{
 
+	//private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+	org.slf4j.Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 	public void afficher(String chaine) {
 		// TODO Auto-generated method stub
 		System.out.println(chaine ) ; 
-		 
+		logger.info(chaine);
 	}
 
 }
